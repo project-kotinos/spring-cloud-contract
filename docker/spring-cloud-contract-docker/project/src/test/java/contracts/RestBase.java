f768ee729ef5689aacb274660fa566dda0b4fca7
Marcin Grzejszczak <marcin.grzejszczak@gmail.com>
2020-06-22 18:04:47 +0200

    WIP

--- docker/spring-cloud-contract-docker/project/src/test/java/contracts/RestBase.java
+++ docker/spring-cloud-contract-docker/project/src/test/java/contracts/RestBase.java
@@ -16,23 +16,33 @@
 
 package contracts;
 
+import java.time.Duration;
+
 import io.restassured.RestAssured;
 import org.junit.Before;
 import org.junit.runner.RunWith;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.cloud.contract.verifier.messaging.boot.AutoConfigureMessageVerifier;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.util.StringUtils;
+import org.springframework.web.client.RestTemplate;
 
 /**
  * @author Marcin Grzejszczak
  */
 @RunWith(SpringRunner.class)
 @SpringBootTest(classes = RestBase.Config.class, webEnvironment = SpringBootTest.WebEnvironment.NONE)
+@AutoConfigureMessageVerifier
 public abstract class RestBase {
+
+	private static final Logger log = LoggerFactory.getLogger(RestBase.class);
 
 	@Value("${APPLICATION_BASE_URL}")
 	String url;
@@ -49,6 +59,22 @@ public abstract class RestBase {
 		if (StringUtils.hasText(this.username)) {
 			RestAssured.authentication = RestAssured.basic(this.username, this.password);
 		}
+	}
+
+	public void triggerMessage(String label) {
+		String url = this.url + "/springcloudcontract/" + label;
+		log.info("Will send a request to [{}] in order to trigger a message", url);
+		restTemplate().postForObject(url, "", String.class);
+	}
+
+	private RestTemplate restTemplate() {
+		RestTemplateBuilder builder = new RestTemplateBuilder()
+				.setConnectTimeout(Duration.ofSeconds(5))
+				.setReadTimeout(Duration.ofSeconds(5));
+		if (StringUtils.hasText(this.username)) {
+			builder = builder.basicAuthentication(this.username, this.password);
+		}
+		return builder.build();
 	}
 
 	@Configuration
